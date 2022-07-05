0cea52ea0d90a051015784230e31d9727a8317ce
Brady Zalasky <brady@cylosoft.com>
2022-07-05 11:34:20 -0500

    add movie details page

--- MovieRecs/main.js
+++ MovieRecs/main.js
@@ -11,11 +11,16 @@ $(document).ready(() => {
     })
 });
 
-function showMovieDetails(id) {
+function showMovieDetails() {
+    let id = sessionStorage.getItem('movieId')
     let url = ''.concat(baseURL, 'movie/' , id, '?api_key=', APIKEY, '&language=en-US');
     fetch (url)
     .then(result => result.json())
     .then((data) => {
+        let output = '';
+        output += '<div class="container"><div class="row" id="movieInfo"><div class="col-sm-4" style="padding-left: 0px;"><div class="card text-center"><img src="' + baseImageURL + data.poster_path + '"></div></div><div class="col-sm-8"><div class="vitals text-center"><h1>' + data.title + '</h1><ul class="list-inline"><li class="list-inline-item">' + data.release_date + '</li><li class="list-inline-item">' + data.runtime + ' min</li></ul></div><p>' + data.overview + '</p><a class="btn btn-primary" href="/MovieRecs/index.html" style="position: absolute; bottom: 25; right: 25;">Back</a></div> </div></div>';
+        
+        $('#movieInfo').html(output);
         console.log(data);
     })
 }
@@ -29,10 +34,15 @@ function getMovies(searchText) {
         let output = '';
         $.each (movies, (index, movie) => {
             output +=
-            '<div class="col-md-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="showMovieDetails(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p></div></div>';
+            '<div class="col-md-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p><a onclick="movieSelected(' + movie.id + ')" class="btn btn-primary" href="#">Movie Details</a></div></div>';
             
             $('#moviesList').html(output);
         })
     })
 }
 
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
