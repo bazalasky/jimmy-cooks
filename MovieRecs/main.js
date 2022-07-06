ecd27f9375323ac34f6e2de5505b752c110854ab
Brady Zalasky <brady@cylosoft.com>
2022-07-06 09:22:02 -0500

    get now playing

--- MovieRecs/main.js
+++ MovieRecs/main.js
@@ -6,9 +6,10 @@ let baseImageURL = 'https://image.tmdb.org/t/p/original/';
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchField').val();
-        getMovies(searchText);
+        getSearch(searchText);
         e.preventDefault();
     })
+    getNowPlaying();
 });
 
 function showMovieDetails() {
@@ -21,11 +22,10 @@ function showMovieDetails() {
         output += '<div class="container"><div class="row" id="movieInfo"><div class="col-sm-4" style="padding-left: 0px;"><div class="card text-center"><img src="' + baseImageURL + data.poster_path + '"></div></div><div class="col-sm-8"><div class="vitals text-center"><h1>' + data.title + '</h1><ul class="list-inline"><li class="list-inline-item">' + data.release_date + '</li><li class="list-inline-item">' + data.runtime + ' min</li></ul></div><p>' + data.overview + '</p><a class="btn btn-primary" href="/MovieRecs/index.html" style="position: absolute; bottom: 25; right: 25;">Back</a></div> </div></div>';
         
         $('#movieInfo').html(output);
-        console.log(data);
     })
 }
 
-function getMovies(searchText) {
+function getSearch(searchText) {
     let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', searchText);
     fetch(url)
     .then(result => result.json())
@@ -45,4 +45,19 @@ function movieSelected(id) {
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
     return false;
+}
+
+function getNowPlaying() {
+    let url = ''.concat(baseURL, 'movie/now_playing?api_key=', APIKEY, '&language=en-us');
+    fetch(url)
+    .then(result => result.json())
+    .then((data) => {
+        let movies = data.results;
+        let output = '';
+        $.each(movies, (index, movie) => {
+            output += '<div class="col-md-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p><a onclick="movieSelected(' + movie.id + ')" class="btn btn-primary" href="#">Movie Details</a></div></div>';
+
+            $('#nowPlaying').html(output);
+        })        
+    })
 }
