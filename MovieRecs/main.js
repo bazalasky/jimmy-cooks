6beaff76fd4f8872624e2dba246d6025afff1555
Brady Zalasky <brady@cylosoft.com>
2022-07-07 12:07:41 -0500

    slick slider setup

--- MovieRecs/main.js
+++ MovieRecs/main.js
@@ -3,25 +3,20 @@ let baseURL = 'https://api.themoviedb.org/3/';
 let configData = null;
 let baseImageURL = 'https://image.tmdb.org/t/p/original/';
 
-$(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
-        let searchText = $('#searchField').val();
-        getSearch(searchText);
-        e.preventDefault();
-    })
-});
-
 function showMovieDetails() {
     let id = sessionStorage.getItem('movieId')
-    let url = ''.concat(baseURL, 'movie/' , id, '?api_key=', APIKEY, '&language=en-US');
-    fetch (url)
-    .then(result => result.json())
-    .then((data) => {
-        let output = '';
-        output += '<div class="container"><div class="row" id="movieInfo"><div class="col-sm-4" style="padding-left: 0px;"><div class="card text-center"><img src="' + baseImageURL + data.poster_path + '"></div></div><div class="col-sm-8"><div class="vitals text-center"><h1>' + data.title + '</h1><ul class="list-inline"><li class="list-inline-item">' + data.release_date + '</li><li class="list-inline-item">' + data.runtime + ' min</li></ul></div><p>' + data.overview + '</p><a class="btn btn-primary" href="/MovieRecs/index.html" style="position: absolute; bottom: 25; right: 25;">Back</a></div> </div></div>';
-        
-        $('#movieInfo').html(output);
-    })
+    if (id) {
+        let url = ''.concat(baseURL, 'movie/' , id, '?api_key=', APIKEY, '&language=en-US');
+        fetch (url)
+        .then(result => result.json())
+        .then((data) => {
+            let output = '';
+            output += '<div class="container"><div class="row" id="movieInfo"><div class="col-sm-4" style="padding-left: 0px;"><div class="card text-center"><img src="' + baseImageURL + data.poster_path + '"></div></div><div class="col-sm-8"><div class="vitals text-center"><h1>' + data.title + '</h1><ul class="list-inline"><li class="list-inline-item">' + data.release_date + '</li><li class="list-inline-item">' + data.runtime + ' min</li></ul></div><p>' + data.overview + '</p><a class="btn btn-primary" href="/MovieRecs/index.html" style="position: absolute; bottom: 25; right: 25;">Back</a></div> </div></div>';
+            
+            $('#movieInfo').html(output);
+        })
+    }
+    
 }
 
 function getSearch(searchText) {
@@ -33,7 +28,7 @@ function getSearch(searchText) {
         let output = '';
         $.each (movies, (index, movie) => {
             output +=
-            '<div class="col-md-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p><a onclick="movieSelected(' + movie.id + ')" class="btn btn-primary" href="#">Movie Details</a></div></div>';
+            '<div class="col-sm-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p><a onclick="movieSelected(' + movie.id + ')" class="btn btn-primary" href="#">Movie Details</a></div></div>';
             
             $('#moviesList').html(output);
         })
@@ -54,9 +49,11 @@ function getNowPlaying() {
         let movies = data.results;
         let output = '';
         $.each(movies, (index, movie) => {
-            output += '<div class="col-md-3" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"><h5>' + movie.title +'</h5><p>' + movie.release_date + '</p><a onclick="movieSelected(' + movie.id + ')" class="btn btn-primary" href="#">Movie Details</a></div></div>';
+            output += '<li class="col" style="margin-top: 20px; margin-bottom: 20px; cursor: pointer;" onclick="movieSelected(' + movie.id + ')"><div class="card text-center"><img src="' + baseImageURL + movie.poster_path + '"></div></div>';
 
             $('#nowPlaying').html(output);
         })        
     })
 }
+
+
